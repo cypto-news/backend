ac417555edd191a9979f925f908d0e504216f189
terkoizmy <dimas.aryan021@gmailcom>
2022-01-14 04:18:52 +0700

    edit models

--- restapi/models.py
+++ restapi/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models.fields import TextField
 
 
 class News(models.Model):
-    title = models.CharField(max_length=60)
-    description = models.CharField(max_length=500)
+    title = models.TextField()
+    description = models.TextField()
     date = models.DateField(max_length=10)
     source = models.CharField(max_length=60)
     rhn =  models.IntegerField(default=1,validators=[MaxValueValidator(10), MinValueValidator(1)])
